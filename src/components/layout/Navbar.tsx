eefe7351b96d52a77b44fe3894a7358c2b69285b
rithythul <rithythul@gmail.com>
2026-02-25 12:20:00 +0700

    fix mega menu flicking

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -213,7 +213,7 @@ const Navbar = () => {
 			<AnimatePresence>
 				{activeMegaItem && activeMegaItem.items && (
 					<motion.div
-						key={activeMegaItem.name}
+						key="mega-panel"
 						initial={{ opacity: 0, y: 8 }}
 						animate={{ opacity: 1, y: 0 }}
 						exit={{ opacity: 0, y: 8 }}
